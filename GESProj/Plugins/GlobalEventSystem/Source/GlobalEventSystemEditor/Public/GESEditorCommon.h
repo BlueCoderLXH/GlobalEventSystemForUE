9600043008e553384c9c67195f2f0bc31b865b40
lixiaohua <bluecoder@yeah.net>
2023-12-04 20:32:13 +0800

    [DispatchEvent] First version

---
+++ GESProj/Plugins/GlobalEventSystem/Source/GlobalEventSystemEditor/Public/GESEditorCommon.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GESEditorCommon.generated.h"
+
+USTRUCT()
+struct FGESEventDataPinInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FName PinName;
+
+	UPROPERTY()
+	FEdGraphPinType PinType;
+};
+
+namespace GESEditorConstant
+{
+	const FName EventTypePinName = TEXT("EventType");
+	const FName EventDataArrayPinName = TEXT("EventDataArray");
+	const FName EventDelegatePinName = TEXT("EventDelegate");
+	const FName OnEventPinName = TEXT("OnEvent");
+	const FString EventDataPinPrefixName = TEXT("EventData");
+	
+}
